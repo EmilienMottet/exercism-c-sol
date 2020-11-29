8f724fb13db80a1dbf9d3d8dfbf8c3c3f3002004
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-11-29 21:41:58 +0100

    complete hello world

--- hello-world/src/hello_world.c
+++ hello-world/src/hello_world.c
@@ -5,10 +5,4 @@
 
 #include "hello_world.h"
 
-// Define the function itself.
-const char *hello(void)
-{
-   // To fix this function, change the return statement here to instead return
-   // a string equivalent to the string expected by the failing test.
-   return NULL;
-}
+const char *hello(void) { return "Hello, World!"; }
