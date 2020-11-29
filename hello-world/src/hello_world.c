7044f6db86341d54b73d4a6c762432de925b1500
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-11-29 21:40:41 +0100

    add .gitignore

--- hello-world/src/hello_world.c
+++ hello-world/src/hello_world.c
@@ -5,4 +5,10 @@
 
 #include "hello_world.h"
 
-const char *hello(void) { return "Hello, World!"; }
+// Define the function itself.
+const char *hello(void)
+{
+   // To fix this function, change the return statement here to instead return
+   // a string equivalent to the string expected by the failing test.
+   return NULL;
+}
